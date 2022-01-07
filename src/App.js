f2fceef76a83402701b7f4f9eda1404b8af017fa
Tanner Causey <tanner.causey@icloud.com>
2022-01-07 16:25:39 -0600

    added apiEndpoint and App state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
-import { Route, Routes } from "react-router-dom";
+//import { Route, Routes } from "react-router-dom";
 import axios from "axios";
 import logo from "./logo.svg";
 import NavBar from "./components/navbar";
 import "./App.css";
 import Card from "./components/card";
 
+const apiEndpoint = "https://pokeapi.co/api/v2/";
+
 class App extends Component {
+	state = {
+		bulbasaur: {},
+	};
+	async componentDidMount() {
+		const { data: bulbasaur } = await axios.get(apiEndpoint + "pokemon-species/1/");
+		console.log(bulbasaur);
+		this.setState(bulbasaur);
+	}
+
 	render() {
 		return (
 			<React.Fragment>
-				<NavBar />
 				<main class="container">
-					<Routes>
-						<Route path="/pokemon" component={Pokemon} />
-					</Routes>
+					<p>{this.state.bulbasaur}</p>
 					<Card />
 				</main>
 			</React.Fragment>
