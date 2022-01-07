626d65c2077d96fecb61cbf82c9c176abcb6ba64
Amber Fenimore <amberfenimore@Ambers-MacBook-Pro.local>
2022-01-07 16:28:12 -0600

    trying again

--- src/App.js
+++ src/App.js
@@ -8,43 +8,19 @@ import Card from "./components/card";
 import RanButton from "./components/ranButton";
 
 class App extends Component {
-<<<<<<< HEAD
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <RanButton />
+      <React.Fragment>
+        <NavBar />
+        <main class="container">
+          <Routes>
+            <Route path="/pokemon" component={Pokemon} />
+          </Routes>
           <Card />
-          {/* <img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer">
-						Learn React
-					</a> */}
-        </header>
-      </div>
+        </main>
+      </React.Fragment>
     );
   }
-=======
-	render() {
-		return (
-			<React.Fragment>
-				<NavBar />
-				<main class="container">
-					<Routes>
-						<Route path="/pokemon" component={Pokemon} />
-					</Routes>
-					<Card />
-				</main>
-			</React.Fragment>
-		);
-	}
->>>>>>> 05b92721aa751c76732ecc2daf41247b8161bfcd
 }
 
 export default App;
