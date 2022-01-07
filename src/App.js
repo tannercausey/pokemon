05b92721aa751c76732ecc2daf41247b8161bfcd
Tanner Causey <tanner.causey@icloud.com>
2022-01-07 16:05:03 -0600

    adding  navbar and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React, { Component } from "react";
+import { Route, Routes } from "react-router-dom";
+import axios from "axios";
 import logo from "./logo.svg";
+import NavBar from "./components/navbar";
 import "./App.css";
 import Card from "./components/card";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Card />
-          {/* <img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer">
-						Learn React
-					</a> */}
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<NavBar />
+				<main class="container">
+					<Routes>
+						<Route path="/pokemon" component={Pokemon} />
+					</Routes>
+					<Card />
+				</main>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
