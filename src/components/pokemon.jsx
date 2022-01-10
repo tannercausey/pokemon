e7bf720facf59f7de058c5161c8406a82c6e403c
tannercausey <tanner.causey@icloud.com>
2022-01-10 10:44:48 -0600

    update, working on more features

--- src/components/pokemon.jsx
+++ src/components/pokemon.jsx
@@ -6,70 +6,80 @@ const apiEndpoint = "https://pokeapi.co/api/v2/";
 class Pokemon extends Component {
 	state = {
 		pokemon: {
-			number: 1,
-			name: "Bulbasaur",
-			pokedex: "A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.",
-			spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+			number: 0, //1,
+			name: "", // "Bulbasaur",
+			pokedex: "", // "A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.",
+			spriteUrl: "", //"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 		},
 	};
 	getPokemon = async () => {
 		const number = getRandomPokedexNumber();
+		let { data: allpokemons } = await axios.get(apiEndpoint + "pokemon?limit=1118");
+		allpokemons = allpokemons.results;
+		for (let pokemon of allpokemons) {
+			console.log(pokemon.name);
+		}
 		const { data: pokedexData } = await axios.get(apiEndpoint + "pokemon-species/" + number);
+		const pokedexEntry = this.getENdexEntry(pokedexData);
 		const { data: spriteData } = await axios.get(apiEndpoint + "pokemon/" + number);
-		/* 
-		const data = await axios.get(apiEndpoint);
-		console.log("data: ", data); */
-		//console.log(pokedexData, spriteData);
 		const name = this.capitalize(pokedexData.name);
+		/* switch (name) {
+			case "nidoran-f":
+				name = "Nidoran ♀";
+				break;
+			case "nidoran-m":
+				name = "Nidoran ♂";
+			case "mime-jr":
+			case "porygon-z":
+			case "type-null":
+			case "jangmo-o":
+			case "hakamo-o":
+			case "kommo-o":
+			case "tapu-koko":
+			case "tapu-lele":
+			case "tapu-bulu":
+			case "tapu-fini":
+			case "mr-rime":
+			
+		} */
 		const pokemon = {
-			pokemon: {
-				number: number,
-				name: name,
-				pokedex: pokedexData.flavor_text_entries[0].flavor_text
-					.replaceAll("\n", " ")
-					.replaceAll("\f", " "),
-				spriteUrl: spriteData.sprites.other["official-artwork"].front_default,
-			},
+			number: number,
+			name: name,
+			pokedex: pokedexEntry.replaceAll("\n", " ").replaceAll("\f", " "),
+			spriteUrl: spriteData.sprites.other["official-artwork"].front_default,
 		};
-		//console.log(pokemon);
 		return pokemon;
 	};
+	getENdexEntry(pokedexData) {
+		console.log(pokedexData);
+		for (let i = 0; i < pokedexData.flavor_text_entries.length; i++) {
+			if (pokedexData.flavor_text_entries[i].language.name === "en") {
+				return pokedexData.flavor_text_entries[i].flavor_text;
+			}
+		}
+	}
 	async componentDidMount() {
 		await this.handleNewPokemonClick();
 	}
 	handleNewPokemonClick = async () => {
 		const pokemon = await this.getPokemon();
-		console.log(pokemon);
 		this.setState({ pokemon });
 	};
 	capitalize(str) {
 		return str.charAt(0).toUpperCase() + str.slice(1);
 	}
 	render() {
-		const { name, number, pokedex, spriteUrl } = this.state;
-		console.log(
-			"pokemon from state: ",
-			name,
-			number,
-			pokedex,
-			spriteUrl
-		);
-		console.log("state", this.state.pokemon);
-		if (this.state.pokemon.pokemon) {console.log(this.state.pokemon.name)}
+		const { name, number, pokedex, spriteUrl } = this.state.pokemon;
 		return (
 			<div>
 				<button onClick={this.handleNewPokemonClick} className="btn btn-primary">
 					Get Random Pokemon
 				</button>
 				<div className="card" style={{ width: 18 + "rem" }}>
-					<img
-						className="card-img-top"
-						src=/* {this.state.pokemon.pokemon.spriteUrl} */""
-						alt="sprite"
-					/>
+					<img className="card-img-top" src={spriteUrl} alt="sprite" />
 					<div className="card-body">
-						<h5 className="card-title">{this.state.pokemon.name ? this.state.pokemon.name :""}</h5>
-						{/* <p className="card-text">{pokemon.pokedex}</p> */}
+						<h5 className="card-title">{name}</h5>
+						{<p className="card-text">{pokedex}</p>}
 					</div>
 				</div>
 			</div>
--- src/utils/random.js
+++ src/utils/random.js
@@ -1,6 +1,5 @@
 const numberOfPokemon = 898; // 898 is the number of pokemon there are
 export function getRandomPokedexNumber() {
 	let number = Math.floor(Math.random() * (numberOfPokemon - 1)) + 1;
-	//console.log(number);
 	return number; // Math.floor(Math.random() * max) gives a range 0..max and the needed range is 1..max+1
 }
